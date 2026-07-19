ff25c5d711e70a7f5696ef0b17669a15b1f339f3
CodeDeff <codedef.main@gmail.com>
2026-07-19 12:54:17 +0530

    Report Now Route changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Signup from './features/auth/pages/Signup'
 import Signin from './features/auth/pages/Signin'
 import WeAreWorking from './components/common/WeAreWorking'
 import UserProfilePage from './features/user/pages/UserProfilePage'
-import ReportNow from './pages/ReportNow'
+import ReportNow from './features/user/pages/ReportNow'
  
 function App() {
   return (
@@ -13,10 +13,10 @@ function App() {
     <div >
        <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route  path='auth/signup' element={<Signup/>} />
-        <Route path='auth/signin' element={<Signin/>} />
+        <Route  path='/auth/signup' element={<Signup/>} />
+        <Route path='/auth/signin' element={<Signin/>} />
         <Route path='/user/home' element={<UserProfilePage />} />
-        <Route path='/report-an-issue-now' element={<ReportNow />} />
+        <Route path='/user/reportnow' element={<ReportNow />} />
         <Route path='*' element={<WeAreWorking/>} />
       </Routes>
     </div>
