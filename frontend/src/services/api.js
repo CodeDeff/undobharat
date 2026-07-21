cda47bc2de05682c263f4a32ec786a64292cd9c2
CodeDeff <codedef.main@gmail.com>
2026-07-21 12:04:41 +0530

    feat: initialize frontend project with gitignore and configured axios API service

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const localhost = "http://localhost:3000/api";
-const production = "https://undobharat.onrender.com";
+const production = import.meta.env.VITE_API_URL;
 const baseURL = process.env.NODE_ENV === "production" ? production : localhost;
 
 const api = axios.create({
@@ -9,6 +9,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
 
 export default api;
