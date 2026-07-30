2fe56265f27d4bc7bbf31d0bb7e5a3b0db0c84b8
CodeDeff <codedef.main@gmail.com>
2026-07-30 18:04:54 +0530

    Page navigating based on User role.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,11 +9,22 @@ import Stats from '../components/homeComponents/StatsUB';
 import Footer from '../components/common/Footer';
 import Navbar from '../components/common/Navbar';
 
+import {getRole} from '../components/homeComponents/services/role.js'
+
 const Home = () => {
   const navigate = useNavigate()
 
   const gotoPage = async () => {
-    navigate('/user/home')
+   try {
+     const respone= await getRole();
+    const role=respone.data.role;
+    if(role === "user") return navigate('/user/home')
+      else alert("Please Login..")
+   } catch (error) {
+    console.log("Error:", error)
+    alert("Unauthorized User!")
+   }
+    // navigate('/user/home')
   }
   return (
     <div>
